02d84cb611e53ce1d13d50d1e9108010313d9d48
Philippe de Bekker <P.M.deBekker@student.tudelft.nl>
2023-05-10 14:49:51 +0200

    Add API doc

--- app.py
+++ app.py
@@ -10,6 +10,46 @@ sentiment_pipeline = pipeline("sentiment-analysis")
 
 @app.route('/sentiment', methods=['POST'])
 def predict():
+    """
+    Make a prediction of the sentiment of a message.
+    ---
+    consumes:
+      - application/json
+    parameters:
+        - name: input_data
+          in: body
+          description: Message to be classified
+          required: True
+          schema:
+            type: object
+            required: 
+              - msg
+            properties:
+                msg:
+                    type: string
+                    example: I am happy
+    responses:
+      200:
+        description: Prediction result
+        schema:
+          type: object
+          properties:
+            sentiment:
+              type: object
+              properties:
+                label:
+                  type: string
+                  enum: ['POSITIVE', 'NEGATIVE']
+                  description: Predicted sentiment label
+                  example: 'POSITIVE'
+                score:
+                  type: number
+                  format: float
+                  minimum: 0
+                  maximum: 1
+                  description: Prediction confidence score
+                  example: 0.9999998807907104
+    """
     msg = request.get_json().get('msg')
     _sentiment = sentiment_pipeline(msg)
 
