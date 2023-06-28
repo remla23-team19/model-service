09568f92616c105487a51ec4b90af85e63af411c
Philippe de Bekker <research@philippedebekker.com>
2023-06-28 03:03:29 +0200

    Use versioned models from model-training

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ WORKDIR /root
 COPY requirements.txt /root/
 RUN pip install -r requirements.txt
 COPY app.py /root/
+COPY model_library.py /root/
+COPY models/ /root/models/
 ENTRYPOINT ["python"]
 CMD ["app.py"]
 EXPOSE 8080
--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, request, jsonify
 from flasgger import Swagger
-from transformers import pipeline
+from flask import Flask, jsonify, request
+
+import model_library as modellib
 
 app = Flask(__name__)
 swagger = Swagger(app)
-sentiment_pipeline = pipeline("sentiment-analysis")
 
 
-@app.route('/sentiment', methods=['POST'])
+@app.route("/sentiment", methods=["POST"])
 def predict():
     """
     Make a prediction of the sentiment of a message.
@@ -21,7 +21,7 @@ def predict():
           required: True
           schema:
             type: object
-            required: 
+            required:
               - msg
             properties:
                 msg:
@@ -49,10 +49,37 @@ def predict():
                   description: Prediction confidence score
                   example: 0.9999998807907104
     """
-    msg = request.get_json().get('msg')
-    _sentiment = sentiment_pipeline(msg)
-    response = jsonify({"sentiment": _sentiment[0]})
-    response.headers.add('Access-Control-Allow-Origin', '*')
+    data = request.get_json()
+    msg = data.get("msg")
+    version = data.get("version") if "version" in data and modellib.verify_version(data.get("version")) and data.get("version") in modellib.get_available_models() else "base"
+
+    sentiment = modellib.predict_sentiment(msg, version)
+    response = jsonify({"sentiment": {
+        "label": modellib.PREDICTION_MAP[sentiment],
+        "score": 1.0
+    }})
+    response.headers.add("Access-Control-Allow-Origin", "*")
     return response
+
+@app.route("/versions", methods=["GET"])
+def get_available_versions():
+    """
+    Get available model versions.
+    ---
+    responses:
+      200:
+        description: List of available model versions
+        schema:
+          type: array
+          items:
+            type: string
+            description: Model version
+            example: v1.0.1
+    """
+    available_versions = modellib.get_available_models()
+    response = jsonify(available_versions)
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
 
 app.run(host="0.0.0.0", port=8080, debug=True)
